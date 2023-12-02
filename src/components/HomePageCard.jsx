ae52fdc3578e5ec274121bd9aaa7783f220b2029
premvarma2002 <vakilverma1982@gmail.com>
2023-12-02 23:06:02 +0530

    Documents well formatted

--- src/components/HomePageCard.jsx
+++ src/components/HomePageCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const HomePageCard = ({title,img,link}) => {
+const HomePageCard = ({ title, img, link }) => {
   return (
     <div className='h-[420px] bg-white z-30 m-3'>
       <div className='text-lg xl:text-xl font-semibold ml-4 mt-4'>{title}</div>

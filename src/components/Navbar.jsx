0ac1e6a0fd0eabb5e9bb6d052297f5adaf7d5f1c
premvarma2002 <vakilverma1982@gmail.com>
2023-12-03 17:57:07 +0530

    importing heroicons from here on

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 <header className='min-w-[1000px]'>
     <div className='flex bg-amazonclone h-[60px] text-white'>
            {/* left  */}
-        <div className='flex'>
+        <div className='flex items-center'>
             <img className='h-[35px] w-[100px] m-2' src='../images/amazon.png' alt=''/>
             <div className='pr-4 pl-4'>
                 <div className='text-xs xl:text-sm'>Deliver to</div>
@@ -15,7 +15,18 @@ const Navbar = () => {
           {/* middle  */}
         <div className='flex'>middle</div>   
           {/* right  */}
-        <div className='flex'>right</div>   
+        <div className='fle items-centerx'></div>
+        <div className='pr-4 pl-4'>
+                <div className='text-xs xl:text-sm'>Hello , Sign in to</div>
+                <div className='text-sm xl:text-base font-bold'>Accounts & Lists</div>
+            </div>
+            <div className='pr-4 pl-4'>
+                <div className='text-xs xl:text-sm'>Returns</div>
+                <div className='text-sm xl:text-base font-bold'>& Orders</div>
+            </div>
+            <div className=' flex pr-3 pl-3'>
+                
+                </div>   
     </div>
 </header>
     )

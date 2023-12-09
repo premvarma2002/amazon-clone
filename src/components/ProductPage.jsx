b8d19dcf0bd1aee4b92ab4baad93765272ab1b7e
premvarma2002 <vakilverma1982@gmail.com>
2023-12-09 11:25:49 +0530

    working on product page

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -21,7 +21,20 @@ const ProductPage = () => {
 
     if (!product?.title) return <h1>Loading Product ...</h1>
   return ( product &&
-    <div>ProductPage {product.title} </div>
+    <div className='h-screen bg-amazonclone-background '>
+      <div className='min-w-[1000px] max-w-[1500px] m-auto bg-orange-400'>
+        <div className='grid grid-cols-10'>
+           {/* {left} */}
+           <div className='col-span-3 p-8 rounded bg-white'>
+            <img src={`${product.image}`} alt="" />
+           </div>
+           {/* {middle } */}
+           <div className='col-span-5 bg-pink-400'></div>
+           {/* {right } */}
+           <div className='col-span-2 bg-green-500'></div>
+        </div>
+      </div>
+    </div>
   )
 }
 

dceba7866ba24ef24423c6d845bc25e11ba99be3
premvarma2002 <vakilverma1982@gmail.com>
2023-12-09 17:58:04 +0530

    ratings work done

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useState , useEffect} from 'react';
+import ProductDetails from './ProductDetails';
 
 import { callAPI } from '../utils/CallApi';
 
@@ -17,21 +18,26 @@ const ProductPage = () => {
 
     useEffect(() => {
       getProduct();
-    },[])
+    },[]);
 
     if (!product?.title) return <h1>Loading Product ...</h1>
   return ( product &&
     <div className='h-screen bg-amazonclone-background '>
       <div className='min-w-[1000px] max-w-[1500px] m-auto bg-orange-400'>
-        <div className='grid grid-cols-10'>
+        <div className='grid grid-cols-10 gap-2'>
            {/* {left} */}
-           <div className='col-span-3 p-8 rounded bg-white'>
+           <div className='col-span-3 p-8 rounded bg-white m-auto'>
             <img src={`${product.image}`} alt="" />
            </div>
            {/* {middle } */}
-           <div className='col-span-5 bg-pink-400'></div>
+           <div className='col-span-5 p-4 rounded bg-white divide-y divide-gray-400 '>
+            <div className='mb-3'><ProductDetails product={product} ratings={true}/></div>
+            <div></div>
+           </div>
            {/* {right } */}
-           <div className='col-span-2 bg-green-500'></div>
+           <div className='col-span-2 bg-green-500'>
+
+           </div>
         </div>
       </div>
     </div>

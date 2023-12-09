0a377efc4eeba103306661d690a9185a4cdb98c3
premvarma2002 <vakilverma1982@gmail.com>
2023-12-09 23:30:22 +0530

    product page half done

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -32,11 +32,25 @@ const ProductPage = () => {
            {/* {middle } */}
            <div className='col-span-5 p-4 rounded bg-white divide-y divide-gray-400 '>
             <div className='mb-3'><ProductDetails product={product} ratings={true}/></div>
-            <div></div>
+            <div className='text-base xl:text-lg mt-3'>
+              {product.description}
+            </div>
            </div>
            {/* {right } */}
-           <div className='col-span-2 bg-green-500'>
-
+           <div className='col-span-2 p-4 rounded bg-white'>
+             <div className='text-xl xl:text-2xl font-semibold'>{product.price}</div>
+             <div className='text-base xl:text-lg font-semibold'>{product.oldPrice}</div>
+             <div className='text-sm xl:text-base font-semibold'>FREE Returns</div>
+             <div className='text-sm xl:text-base font-semibold'>FREE Delivery</div>
+             <div className='text-base xl:text-lg font-semibold'>In Stock</div>
+             <div className='text-base xl:text-lg'>Quantity:
+              <select>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+              </select>
+             </div>
+             <button>Add to cart</button>
            </div>
         </div>
       </div>
